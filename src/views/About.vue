8b74d03227e3bcd636b86d7b7ce21ddbb4091037
benjaminortizulloa <plzbeemyfriend@gmail.com>
2020-07-27 02:15:25 -0500

    lorem ipsumed about

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,6 @@
   <v-card flat class="about">
     <v-img :src="require('../assets/Placeholder.png')" class="my-3" contain height="200"/>
     <h2>This is an about page</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum quam libero magnam molestiae iure et impedit alias vitae facilis. Sapiente nemo animi expedita pariatur libero rerum, ea culpa illum necessitatibus!</p>
   </v-card>
 </template>
