2eb394f9424eac4e238b9caf7ca3bffac63f9c1e
benjaminortizulloa <plzbeemyfriend@gmail.com>
2020-10-04 20:10:46 -0500

    changed contact email

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,7 @@
             <v-btn icon href="https://github.com/reconhub" target="_blank">
               <v-icon>fa4 fa-github fa-inverse</v-icon>
             </v-btn>
-            <v-btn icon href="mailto:repidemicsconsortium@gmail.com" target="_blank">
+            <v-btn icon href="mailto:mj.rolland@gmail.com" target="_blank">
               <v-icon>fa4 fa-envelope fa-inverse</v-icon>
             </v-btn>
           </v-card-text>
